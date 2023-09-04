fb0b464e6a1f7aafe32389553c216b5b510ecdf8
IamVDD <vodinhdat03@gmail.com>
2023-09-04 12:07:09 +0700

    update trainer

--- src/model/trainer.py
+++ src/model/trainer.py
@@ -112,7 +112,8 @@ class Trainer:
         }
         try:
             file_path = f"{self.model_path}/model_{self.config['encoder']['type']}_{self.config['num_epochs']}.pt"
+            torch.save(save_dict, file_path)
         except:
             file_path = f"model_{self.config['encoder']['type']}_{self.config['num_epochs']}.pt"
-        torch.save(save_dict, file_path)
+            torch.save(save_dict, file_path)
 
