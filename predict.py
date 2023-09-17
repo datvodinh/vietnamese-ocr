4d475d006d06da0ec0bb4604d7a62c886c79fcce
IamVDD <vodinhdat03@gmail.com>
2023-09-17 20:01:42 +0700

    add cli train and predict

--- predict.py
+++ predict.py
@@ -5,11 +5,17 @@ import matplotlib.pyplot as plt
 import os
 import torch
 parser = argparse.ArgumentParser()
-parser.add_argument("--model_path",required=True)
+parser.add_argument("--model_path",required=False)
+parser.add_argument("--type",required=True)
 parser.add_argument("--img_path",required=True)
 
 arg = parser.parse_args()
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-infer = Inference(MODEL_PATH  = arg.model_path,device=device)
-
-print(infer.predict(img_dir=arg.img_path))
+if arg.model_path is not None:
+    infer = Inference(MODEL_PATH  = arg.model_path,device=device)
+else:
+    infer = Inference(MODEL_PATH  = "./checkpoint/model.pt",device=device)
+if arg.type == "single":
+    print(infer.predict(img_dir=arg.img_path))
+elif arg.type == "batch":
+    infer.predict_batch(arg.img_path)
---
+++ train.py
@@ -0,0 +1,37 @@
+import torch
+import os
+import argparse
+import yaml
+from src.model.trainer import Trainer
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--src",help="Training image path")
+parser.add_argument("--target",help="Training label path")
+parser.add_argument("--model",help="Model save path")
+parser.add_argument("--config",help="Config save path")
+arg = parser.parse_args()
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+if arg.config is not None:
+    f_name = arg.config
+else:
+    f_name = "config/swin_config.yaml"
+
+with open(f_name,"r") as f:
+    c = f.read()
+config = yaml.safe_load(c)
+
+config['augmentation'] = True
+config['print_type'] = "per_batch"
+
+trainer = Trainer(config      = config,
+                IMAGE_PATH  = arg.src,
+                TARGET_PATH = arg.target,
+                MODEL_PATH  = arg.model,
+                device      = device)
+    
+if __name__ == "__main__":
+    
+    print(config)
+    trainer.train()
